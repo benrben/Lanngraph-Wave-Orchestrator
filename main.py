ca3efa87e968ddc5f5333860e9d83451245bc2ca
Ben R <benrben4@gmail.com>
2025-07-01 19:48:56 +0300

    Sure! Pl

---
+++ main.py
@@ -0,0 +1,14 @@
+from src import WaveOrchestrator
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def main():
+    wave_orchestrator = WaveOrchestrator(llm=ChatOpenAI(model="gpt-4o-mini", temperature=0.0))
+
+    print("Hello from waveorchestrator!")
+
+
+if __name__ == "__main__":
+    main()
