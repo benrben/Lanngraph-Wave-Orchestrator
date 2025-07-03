1c634d2de73e9deec6fa6b10bca8fc3863c821c3
Ben R <benrben4@gmail.com>
2025-07-04 00:35:04 +0300

    Bump version to 1.1.7 in pyproject.toml and remove unnecessary state initialization in create_sequential_progress_node method of WaveOrchestrator for cleaner code and improved state management.

--- langgraph_wave_orchestrator/wave_orchestrator.py
+++ langgraph_wave_orchestrator/wave_orchestrator.py
@@ -44,10 +44,7 @@ class WaveOrchestrator:
     def create_sequential_progress_node(self):
         def sequential_progress(state: any) -> Command[Literal[*self.worker_manager.workers,"answering"]]:
             print(f"sequential_progress")
-            state.current_wave = 0
-            state.execution_waves.waves =  {}
-            state.task_plans = ParallelTasksPlans(task_plans=[])
-            print(f"current wave: {state.current_wave}")
+            
             update = self.state_manager.prepare_command_output(state)
             print(f"length of waves: {len(state.execution_waves.waves)} current wave: {state.current_wave}")
             if self.wave_manager.is_waves_complete(state):
